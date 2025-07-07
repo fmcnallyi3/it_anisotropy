7ea71047a8681eeb8347936c5a43a6f9c073e97c
tfutrell <tommyfutrell@icloud.com>
2025-07-07 14:48:48 +0000

    Simplified save_data, added documentation

--- rigidity/save_data.py
+++ rigidity/save_data.py
@@ -1,4 +1,8 @@
-import os, sys, glob, tables, argparse
+# Requires cvmfs
+import os, sys, tables, argparse
+
+from glob import glob
+
 import numpy as np
 # pip install simweights <--- run this once to install the package
 # then replace <username> with your username and <version> with your Python version
@@ -6,62 +10,48 @@ sys.path.append('/home/tfutrell/.local/lib/python3.12/site-packages')
 import simweights
 
 def main(args):
+    # Set up the base directory for the files
     dir_base = f'/data/ana/CosmicRay/IceTop_level3/sim/IC86.{args.year}/{args.model}/'
     dir_end = '/h5files/*.h5'
 
+    # Match the year and simulation to a list of files - if-elif statements because default cvmfs does not support match-case statements
+    # If you want to add a year or simulation, add an (el)if args.year == <year>: line, then if an args.model == <model>: line, and then
+    # a line file_list = glob(<Fe_directory>) + glob(<He_directory>) + glob(<O_directory>) + glob(<p_directory>)
     if args.year == 2012:
         if args.model == 'EPOS-LHC':
-            fe_list = glob.glob(dir_base + 'Fe/12635_v1s' + dir_end)
-            he_list = None
-            o_list = None
-            p_list = glob.glob(dir_base + 'p/12634_v1s' + dir_end)
+            file_list = glob(dir_base + 'Fe/12635_v1s' + dir_end) + glob(dir_base + 'p/12634_v1s' + dir_end)
         
         elif args.model == 'SIBYLL2.1':
-            fe_list = glob.glob(dir_base + 'Fe/12362_v1s' + dir_end) # Can be combined with 20144_v1s
-            he_list = glob.glob(dir_base + 'He/12630_v1s' + dir_end) # Can be combined with 20145_v1s
-            o_list = glob.glob(dir_base + 'O/12631_v1s' + dir_end) # Can be combined with 20146_v1s
-            p_list = glob.glob(dir_base + 'p/12360_v1s' + dir_end) # Can be combined with 20143_v1s
+            file_list = glob(dir_base + 'Fe/12362_v1s' + dir_end) # Can be combined with 20144_v1s
+            file_list += glob(dir_base + 'He/12630_v1s' + dir_end) # Can be combined with 20145_v1s
+            file_list += glob(dir_base + 'O/12631_v1s' + dir_end) # Can be combined with 20146_v1s
+            file_list += glob(dir_base + 'p/12360_v1s' + dir_end) # Can be combined with 20143_v1s
         
         elif args.model == 'SIBYLL2.3':            
-            fe_list = glob.glob(dir_base + 'Fe/12633_v1s' + dir_end)
-            he_list = None
-            o_list = None
-            p_list = glob.glob(dir_base + 'p/12632_v1s' + dir_end)
+            file_list = glob(dir_base + 'Fe/12633_v1s' + dir_end) + glob(dir_base + 'p/12632_v1s' + dir_end)
 
     elif args.year == 2015:    
-        fe_list = glob.glob(dir_base + 'Fe/20180_v1s' + dir_end)
-        he_list = glob.glob(dir_base + 'He/20178_v1s' + dir_end)
-        o_list = glob.glob(dir_base + 'O/20179_v1s' + dir_end)
-        p_list = glob.glob(dir_base + 'p/20174_v1s' + dir_end)
+        file_list = glob(dir_base + 'Fe/20180_v1s' + dir_end) + glob(dir_base + 'He/20178_v1s' + dir_end) + glob(dir_base + 'O/20179_v1s' + dir_end) + glob(dir_base + 'p/20174_v1s' + dir_end)
 
+    # 2018 is a little quirky, so it needs to be treated differently
     elif args.year == 2018:
         if args.model == 'EPOS-LHC':
-            # Missing IceTopHLCSeedRTPulses_SnowUnAttenuated_info in Fe/23201_v3/h5files/...5.0
-            fe_list = []
-            he_list = []
-            o_list = []
-            p_list = []
+            file_list = []
             for i in range(51, 69):
-                fe_list += glob.glob(dir_base + 'Fe/23201_v3' + f'/h5files/*E{i/10}*.h5')
-                he_list += glob.glob(dir_base + 'He/23199_v3' + f'/h5files/*E{i/10}*.h5')
-                o_list += glob.glob(dir_base + 'O/23200_v3' + f'/h5files/*E{i/10}*.h5')
-                p_list += glob.glob(dir_base + 'p/23198_v3' + f'/h5files/*E{i/10}*.h5')
+                file_list += glob(dir_base + 'Fe/23201_v3' + f'/h5files/*E{i/10}*.h5') + glob(dir_base + 'He/23199_v3' + f'/h5files/*E{i/10}*.h5') + glob(dir_base + 'O/23200_v3' + f'/h5files/*E{i/10}*.h5') + glob(dir_base + 'p/23198_v3' + f'/h5files/*E{i/10}*.h5')
         
         elif args.model == 'SIBYLL2.3d':
-            fe_list = []
-            he_list = []
-            o_list = []
-            p_list = []
+            file_list = []
             for i in range(48, 79):
-                fe_list = glob.glob(dir_base + 'Fe_allE_links_v3' + f'/h5files/*E{i/10}*.h5')
-                he_list = glob.glob(dir_base + 'He_allE_links_v3' + f'/h5files/*E{i/10}*.h5')
-                o_list = glob.glob(dir_base + 'O_allE_links_v3' + f'/h5files/*E{i/10}*.h5')
-                p_list = glob.glob(dir_base + 'p_allE_links_v3' + f'/h5files/*E{i/10}*.h5')
+                file_list += glob(dir_base + 'Fe_allE_links_v3' + f'/h5files/*E{i/10}*.h5') + glob(dir_base + 'He_allE_links_v3' + f'/h5files/*E{i/10}*.h5') + glob(dir_base + 'O_allE_links_v3' + f'/h5files/*E{i/10}*.h5') + glob(dir_base + 'p_allE_links_v3' + f'/h5files/*E{i/10}*.h5')
 
+    # Set up the weighter
     weighter = None
-    print('Getting iron weights...')
+    print('Getting weights, this can take up to two hours...')
 
-    for filename in fe_list:
+    # Loop through every single file and load the weights for it
+    # This is the longest, most resource-intensive part of the process
+    for filename in file_list:
         file_obj = tables.open_file(filename, 'r')
         
         if weighter is None:
@@ -69,56 +59,29 @@ def main(args):
         else:
             weighter += simweights.IceTopWeighter(file_obj)
 
-    print('Got iron weights!')
+    print('Got weights!')
 
-    if not he_list == None:
-        print('Getting helium weights...')
-
-        for filename in he_list:
-            file_obj = tables.open_file(filename, 'r')
-            weighter += simweights.IceTopWeighter(file_obj)
-
-        print('Got helium weights!')
-
-    if not o_list == None:
-        print('Getting oxygen weights...')
-            
-            
-        for filename in o_list:
-            file_obj = tables.open_file(filename, 'r')
-            weighter += simweights.IceTopWeighter(file_obj)
-
-        print('Got oxygen weights!')
-
-    print('Getting proton weights...')
-
-    for filename in p_list:
-        file_obj = tables.open_file(filename, 'r')
-        weighter += simweights.IceTopWeighter(file_obj)
-
-    print('Got proton weights!')
-
-    # If the file path does not exist, create it
+    # If the file path for the year and simulation does not exist, create it
     if not os.path.exists(f'saved_data/{args.year}/{args.model}'):
         os.makedirs(f'saved_data/{args.year}/{args.model}/')
 
-    # Save energy
-    if not os.path.isfile(f'saved_data/{args.year}/{args.model}/energy.npy'):
-        print('Saving energy...')
-        with open(f'saved_data/{args.year}/{args.model}/energy.npy', 'wb') as f:
-            np.save(f, weighter.get_column('MCPrimary', 'energy'))
-        print('Saved!')
-    else:
-        print('File already exists, skipping')
+    # Set up a dictionary for harvesting data - weights are harvested seperately
+    # If you need more data, add it here
+    DATA = {
+        'MCPrimary': ['energy', 'type', 'zenith'],
+        'IceTopHLCSeedRTPulses_SnowUnAttenuated_info': ['nstrings'],
+        'IT73AnalysisIceTopQualityCuts': ['IceTop_reco_succeeded']
+    }
 
-    # Save particle type
-    if not os.path.isfile(f'saved_data/{args.year}/{args.model}/particle_type.npy'):
-        print('Saving particle type...')
-        with open(f'saved_data/{args.year}/{args.model}/particle_type.npy', 'wb') as f:
-            np.save(f, weighter.get_column('MCPrimary', 'type'))
-        print('Saved!')
-    else:
-        print('File already exists, skipping')
+    # Save data from DATA
+    for row, sets in DATA.items():
+        for column in sets:
+            if not os.path.isfile(f'saved_data/{args.year}/{args.model}/{column}.npy'):
+                print(f'Saving {column}')
+                with open(f'saved_data/{args.year}/{args.model}/{column}.npy', 'wb') as f:
+                    np.save(f, weighter.get_column(row, column))
+
+                print('Saved!')
 
     # Save H4a weights
     if not os.path.isfile(f'saved_data/{args.year}/{args.model}/Hweights.npy'):
@@ -138,36 +101,11 @@ def main(args):
     else:
         print('File already exists, skipping')
 
-    # Save hits
-    if not os.path.isfile(f'saved_data/{args.year}/{args.model}/hits.npy'):
-        print('Saving hits...')
-        with open(f'saved_data/{args.year}/{args.model}/hits.npy', 'wb') as f:
-            np.save(f, weighter.get_column('IceTopHLCSeedRTPulses_SnowUnAttenuated_info', 'nstrings').astype(int))
-        print('Saved!')
-    else:
-        print('File already exists, skipping')
-
-    # Save reconstruction passed cut
-    if not os.path.isfile(f'saved_data/{args.year}/{args.model}/reco_pass.npy'):
-        print('Saving reco_pass...')
-        with open(f'saved_data/{args.year}/{args.model}/reco_pass.npy', 'wb') as f:
-            np.save(f, weighter.get_column('IT73AnalysisIceTopQualityCuts', 'IceTop_reco_succeeded').astype(bool))
-        print('Saved!')
-    else:
-        print('File already exists, skipping')
-
-    # Save zenith cut
-    if not os.path.isfile(f'saved_data/{args.year}/{args.model}/zenith_cut.npy'):
-        print('Saving zenith cut...')
-        with open(f'saved_data/{args.year}/{args.model}/zenith_cut.npy', 'wb') as f:
-            np.save(f, weighter.get_column('MCPrimary', 'zenith') < np.radians(55))
-        print('Saved!')
-    else:
-        print('File already exists, skipping')
-
 if __name__ == '__main__':
+    # Define the arguments for the file
     parser = argparse.ArgumentParser(description="Save data to a .npy file")
 
+    # Include notes about available years and models here
     parser.add_argument('-y', '--year', type=int, required=True, help='2012, 2015, 2018')
     parser.add_argument('-m', '--model', type=str, required=True, help='(Depending on year) EPOS-LHC, SIBYLL2.1, SIBYLL2.3, SIBYLL2.3d')
 
