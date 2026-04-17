0bdfee148172eec7e3400716dcda74e4012c27af
Frank McNally <mcnally_ft@mercer.edu>
2026-04-17 08:07:48 -0500

    Small updates for stability project

--- stability/dt_maker.py
+++ stability/dt_maker.py
@@ -2,12 +2,10 @@
 
 from pathlib import Path
 import numpy as np
-import subprocess
 import getpass
 import argparse
 
 from submitter.pysubmit import pysubmit
-from dt_extractor import dt_extractor
 
 if __name__ == "__main__":
 
@@ -20,44 +18,48 @@ if __name__ == "__main__":
             help='Submit a small sample to the cluster to test')
     args = p.parse_args()
 
-    # Determine paths to root files
-    root_dir = '/data/ana/CosmicRay/Anisotropy/IceTop/ITpass2'
-    root_files = sorted([p for p in Path(root_dir).glob('l3_*.root')])
-
     # Establish path for output, creating directory if it doesn't exist
     user = getpass.getuser()
     out_dir  = Path(f'/data/user/{user}/stability/icetop_dt')
     out_dir.mkdir(parents=True, exist_ok=True)
 
-    # Split root files into jobs for submission (<10 mins per job)
-    njobs = 50
-    sub_lists = np.array_split(root_files, njobs, axis=0)
-    if args.test:
-        sub_lists = sub_lists[:1]
+    # Determine paths to root files
+    root_dir = '/data/ana/CosmicRay/Anisotropy/IceTop/ITpass2'
 
-    for i, root_files in enumerate(sub_lists):
+    for detector_year in range(2011, 2021):
 
-        # Check to see if output file already exists
-        out_file = out_dir / f'hist_{i:03d}.json'
-        if out_file.exists() and not args.overwrite and not args.test:
-            continue
+        root_files = sorted([p for p in
+            Path(root_dir).glob(f'l3_data_run_config_{detector_year}*.root')])
 
-        # Change output name and shorten file list if running as test
+        # Split root files into jobs for submission (<10 mins per job)
+        njobs = 5
+        sub_lists = np.array_split(root_files, njobs, axis=0)
         if args.test:
-            out_file = out_dir / 'hist_test.json'
-            root_files = root_files[:3]
+            sub_lists = sub_lists[:1]
 
-        # Command to run
-        cwd = Path(__file__).parent.resolve()
-        cmd = f'{cwd}/dt_extractor.py'
+        for i, root_files in enumerate(sub_lists):
+    
+            # Check to see if output file already exists
+            out_file = out_dir / f'hist_{i:03d}.json'
+            if out_file.exists() and not args.overwrite and not args.test:
+                continue
 
-        # Input root files as a string
-        file_str = ' '.join([str(f) for f in root_files])
+            # Change output name and shorten file list if running as test
+            if args.test:
+                out_file = out_dir / 'hist_test.json'
+                root_files = root_files[:3]
 
-        # Combined executable with arguments
-        ex = f'{cmd} -f {file_str} -o {out_file}'
+            # Command to run
+            cwd = Path(__file__).parent.resolve()
+            cmd = f'{cwd}/dt_extractor.py'
 
-        # Submit to cluster
-        jobID = f'dt_{out_file.name}'
-        pysubmit(ex, jobID=jobID)
+            # Input root files as a string
+            file_str = ' '.join([str(f) for f in root_files])
+
+            # Combined executable with arguments
+            ex = f'{cmd} -f {file_str} -o {out_file}'
+
+            # Submit to cluster
+            jobID = f'dt_{out_file.name}'
+            pysubmit(ex, jobID=jobID)
 
